1d56ae7dc6b4fa58f5258a2c9bcf25a90a9c09af
Justin <jnatmorris@gmail.com>
2023-04-26 22:27:47 +0000

    add string conversion as moved logic out of wrapper

--- backend/server/src/utils/getNumMols_u.ts
+++ backend/server/src/utils/getNumMols_u.ts
@@ -1,7 +1,7 @@
 import { readFileSync_w } from "../wrappers/readFileSync_w";
 
 export const getNumMols_u = (energyFilePath: string): number => {
-    const readContent = readFileSync_w(energyFilePath);
+    const readContent = readFileSync_w(energyFilePath).toString("utf-8");
 
     const numFilesInDir = readContent.split("\n").length - 1;
 
