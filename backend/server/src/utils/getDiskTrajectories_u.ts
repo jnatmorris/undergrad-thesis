23dae7795984e7ea529e22d0ed4dbbe5c40a25b6
Justin <jnatmorris@gmail.com>
2023-04-26 19:15:32 +0000

    rename

---
+++ backend/server/src/utils/getDiskTrajectories_u.ts
@@ -0,0 +1,44 @@
+import { getTrajectories_t } from "../types/types";
+import { getNumMols_u } from "./getNumMols_u";
+import { readdirSync_w } from "../wrappers/readdirSync_w";
+import { statSync_w } from "../wrappers/statSync_w";
+import { existsSync_w } from "../wrappers/existsSync_w";
+
+export const getDiskTrajectories_u = (
+    trajectoriesDiskPath: string
+): getTrajectories_t[] => {
+    // read directory
+    const trajectoriesAndData = readdirSync_w(trajectoriesDiskPath, {
+        withFileTypes: false,
+    }).map((trajectoryNameStr) => {
+        // get the name of the trajectories
+        const moleculeFolderPath = `${trajectoriesDiskPath}/${trajectoryNameStr}`;
+
+        // test what files inside of the molecule folder
+        const filesInside = readdirSync_w(moleculeFolderPath);
+
+        const filePath = `${trajectoriesDiskPath}/${trajectoryNameStr}/energies.txt`;
+
+        if (existsSync_w(filePath)) {
+            // get the number of molecules
+            const numMolecule = getNumMols_u(filePath);
+
+            // get the time of file creation
+            const { birthtime } = statSync_w(filePath);
+
+            return {
+                moleculeName: trajectoryNameStr,
+                filesInside,
+                numMolecule,
+                uploadDate: birthtime.toISOString(),
+                partialPath: moleculeFolderPath,
+            };
+        }
+    });
+
+    const trajectoriesValid = trajectoriesAndData.filter(
+        (x) => x !== undefined
+    ) as getTrajectories_t[];
+
+    return trajectoriesValid;
+};
