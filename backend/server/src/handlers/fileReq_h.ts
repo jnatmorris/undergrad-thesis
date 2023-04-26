98eac79730b1abdb24072d41ff66657dea059b9b
Justin <jnatmorris@gmail.com>
2023-04-26 19:09:29 +0000

    added handler for file requests

--- backend/server/src/handlers/fileReq_h.ts
+++ backend/server/src/handlers/fileReq_h.ts
@@ -1,13 +1,15 @@
-import { readFileSync } from "fs";
+import { readFileSync_w } from "../wrappers/readFileSync_w";
+import path from "path";
 import type { DefaultEventsMap } from "socket.io/dist/typed-events";
 import type { Socket } from "socket.io";
-import path from "path";
 
 export const reqFile = (
     socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
     filePath: string
 ) => {
-    const file = readFileSync(filePath);
+    // read file using wrapper function to catch errors
+    const file = readFileSync_w(filePath);
+
     socket.emit("resFile", {
         fileName: path.basename(filePath),
         file: file,
