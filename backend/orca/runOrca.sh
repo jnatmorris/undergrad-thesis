38f49b35aed9a74fd1317fd61a6ba60ae18b40c3
Justin <jnatmorris@gmail.com>
2023-04-26 23:04:20 +0000

    fixed to correct grep values

--- backend/orca/runOrca.sh
+++ backend/orca/runOrca.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# ==================================================
 # run orca
+
 /opt/orca_5_0_3_linux_x86-64_openmpi411/orca $1 > $2
 
+# ==================================================
 # grab the relivent energies from the .log file
+
+# Excited Energy
 cat $2 | grep "Lowest Energy" | tail -n1 | cut -d' ' -f20
-cat $2 | grep "Maximum Energy change" | tail -n1 | cut -d' ' -f13
+# Ground state
+cat $2 | grep "E(SCF)" | cut -c 19-30
