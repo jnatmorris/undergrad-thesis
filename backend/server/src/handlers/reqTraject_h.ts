6fa83502fd54f0d213784f90263e50899c03b1cc
Justin <jnatmorris@gmail.com>
2023-04-26 19:10:12 +0000

    added handler for requesting trajectories on disk

--- backend/server/src/handlers/reqTraject_h.ts
+++ backend/server/src/handlers/reqTraject_h.ts
@@ -1,4 +1,4 @@
-import { getTrajectories } from "../utils/getTrajectories";
+import { getDiskTrajectories_u } from "../utils/getDiskTrajectories_u";
 import type { DefaultEventsMap } from "socket.io/dist/typed-events";
 import type { Server } from "socket.io";
 
@@ -6,6 +6,6 @@ export const reqTraject_h = (
     io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
     trajectoriesDiskPath: string
 ) => {
-    const trajectoriesObj = getTrajectories(trajectoriesDiskPath);
+    const trajectoriesObj = getDiskTrajectories_u(trajectoriesDiskPath);
     io.emit("resTrajectories", JSON.stringify(trajectoriesObj));
 };
