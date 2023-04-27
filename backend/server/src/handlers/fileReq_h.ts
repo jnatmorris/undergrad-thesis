c33f2ca52cbc9b893307fa0bfa0ff913b3f44324
Justin <jnatmorris@gmail.com>
2023-04-27 09:50:54 +0000

    Added event types and comments

--- backend/server/src/handlers/fileReq_h.ts
+++ backend/server/src/handlers/fileReq_h.ts
@@ -1,10 +1,15 @@
 import { readFileSync_w } from "../wrappers/readFileSync_w";
 import { basename, resolve } from "path";
-import type { DefaultEventsMap } from "socket.io/dist/typed-events";
+import type {
+    CToSEvents_t,
+    SToCEvents_t,
+    InSEvents_t,
+    SocketData_t,
+} from "../types/types";
 import type { Socket } from "socket.io";
 
 export const reqFile = (
-    socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
+    socket: Socket<CToSEvents_t, SToCEvents_t, InSEvents_t, SocketData_t>,
     reqFilePath: string,
     trajectoriesDiskPath_i: string
 ) => {
@@ -18,15 +23,16 @@ export const reqFile = (
     /* 
         Double check and ensure that the path created resolves in the path 
         specified as directory to all trajectories. Theoretically always
-        should, but does not hear to check
+        should, but does not hurt to check.
     */
     if (!resolve(fullPath).startsWith(resolve(trajectoriesDiskPath_i))) return;
 
     // read file using wrapper function to catch errors
-    const file = readFileSync_w(fullPath);
+    const readFile = readFileSync_w(fullPath);
 
+    // emit to socket the file and filename
     socket.emit("resFile", {
         fileName: basename(reqFilePath),
-        file: file,
+        file: readFile,
     });
 };
