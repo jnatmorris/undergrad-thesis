4ee10116a2bc42e8cbdeb121aebd1666a0ec90e7
Justin <jnatmorris@gmail.com>
2023-04-27 10:56:46 +0000

    update include paths

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Layout } from "../other/layout";
+import { Layout } from "../content/layout";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
     return (
         // put everything in layout component
         <Layout>
             <Component {...pageProps} />
         </Layout>
     );
-}
+};
+
+export default App;
