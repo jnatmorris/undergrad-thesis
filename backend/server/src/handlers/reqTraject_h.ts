c7b375aacf4484a4117ddbe889c3cbf003f1f582
Justin <jnatmorris@gmail.com>
2023-04-27 09:51:04 +0000

    Added event types and comments

--- backend/server/src/handlers/reqTraject_h.ts
+++ backend/server/src/handlers/reqTraject_h.ts
@@ -1,11 +1,19 @@
 import { getDiskTrajectories_u } from "../utils/getDiskTrajectories_u";
-import type { DefaultEventsMap } from "socket.io/dist/typed-events";
 import type { Server } from "socket.io";
+import type {
+    CToSEvents_t,
+    SToCEvents_t,
+    InSEvents_t,
+    SocketData_t,
+} from "../types/types";
 
 export const reqTraject_h = (
-    io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
-    trajectoriesDiskPath: string
+    io: Server<CToSEvents_t, SToCEvents_t, InSEvents_t, SocketData_t>,
+    storageDiskPath: string
 ) => {
-    const trajectoriesObj = getDiskTrajectories_u(trajectoriesDiskPath);
+    // get trajectories on disk
+    const trajectoriesObj = getDiskTrajectories_u(storageDiskPath);
+
+    // emit trajectories to all connected
     io.emit("resTrajectories", JSON.stringify(trajectoriesObj));
 };
