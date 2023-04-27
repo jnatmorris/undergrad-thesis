dbf96c2b398caa8ed569eecb6d417d3c04c4f715
Justin <jnatmorris@gmail.com>
2023-04-27 09:53:26 +0000

    add ocmments

--- backend/server/src/utils/getNumMols_u.ts
+++ backend/server/src/utils/getNumMols_u.ts
@@ -1,9 +1,11 @@
 import { readFileSync_w } from "../wrappers/readFileSync_w";
 
 export const getNumMols_u = (energyFilePath: string): number => {
+    // get the number of molecules by reading the energy file. Convert to string as wrapper returns Buffer
     const readContent = readFileSync_w(energyFilePath).toString("utf-8");
 
-    const numFilesInDir = readContent.split("\n").length - 1;
+    // the number of molecules in file will be the length minus 1 as a new line is always written after last
+    const numMols = readContent.split("\n").length - 1;
 
-    return numFilesInDir;
+    return numMols;
 };
